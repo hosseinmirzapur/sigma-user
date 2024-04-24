25da054a663110ced7429bc1ba036a46c1cf41dd
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-04-24 22:27:06 +0330

    e-learn section improved

--- src/pages/e-learn/index.tsx
+++ src/pages/e-learn/index.tsx
@@ -19,14 +19,7 @@ const ELearn = () => {
 
   return (
     <TabContext value={`${tab}`}>
-      <Tabs
-        variant='scrollable'
-        scrollButtons
-        allowScrollButtonsMobile
-        value={tab}
-        onChange={handleTabChange}
-        className=''
-      >
+      <Tabs variant='scrollable' scrollButtons allowScrollButtonsMobile value={tab} onChange={handleTabChange}>
         {categories.map((category, i) => (
           <Tab key={i} label={category.title} />
         ))}
@@ -34,18 +27,19 @@ const ELearn = () => {
       {categories.map((category, index) => (
         <TabPanel value={`${index}`} key={index} className='grid grid-cols-1 md:grid-cols-2 gap-4'>
           {category.videos.map((video, index) => (
-            <Card key={index} className='flex justify-center items-center'>
-              <div className='flex flex-col lg:flex-row gap-3 md:gap-6 lg:gap-9'>
-                <div className='p-2 w-[300px] mx-auto flex justify-center items-center'>
-                  <ReactPlayer url={video.src} width={300} height={250} />
-                </div>
-                <div className='flex flex-col px-5'>
-                  <div className='h-[100px] pt-0 md:pt-5 lg:pt-10'>
-                    <h1 className='md:text-lg font-bold'>{video.title}</h1>
-                  </div>
-                  <div className='pb-10'>
-                    <p>{video?.description}</p>
-                  </div>
+            <Card key={index} className='w-full md:w-11/12 mx-auto'>
+              <div className='flex flex-col gap-3 md:gap-6 lg:gap-9'>
+                <ReactPlayer
+                  controls
+                  url={video.src}
+                  style={{
+                    padding: '20px 20px'
+                  }}
+                  width={'100%'}
+                />
+                <div className='flex flex-col p-5 gap-5'>
+                  <h1 className='md:text-lg font-bold'>{video.title}</h1>
+                  <p>{video?.description}</p>
                 </div>
               </div>
             </Card>
